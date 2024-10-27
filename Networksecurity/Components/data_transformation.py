8f20f87c8d6feddc8da94fe5fcb690af079c9e01
Syed Haque <wassi244255@gmail.com>
2024-10-27 22:53:25 +0100

    fastapi app + training pipeline for automation completed

--- Networksecurity/Components/data_transformation.py
+++ Networksecurity/Components/data_transformation.py
@@ -94,6 +94,9 @@ class DataTransformation:
             save_numpy_array_data(self.data_transformation_config.transformed_train_file_path, array=train_arr)
             save_numpy_array_data(self.data_transformation_config.transformed_test_file_path, array=test_arr)
             save_object(self.data_transformation_config.transformed_object_file_path, preprocessor_object)
+            #saving preprocessing object for final_models folder         
+
+            save_object("final_model/preprocessor.pkl", preprocessor_object)
 
             #preparing artifacts
 
--- Networksecurity/Components/model_trainer.py
+++ Networksecurity/Components/model_trainer.py
@@ -120,6 +120,8 @@ class ModelTrainer:
         Network_Model = NetworkModel(preprocessor=preprocessor, model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path, obj=Network_Model)
 
+        #save for final_models folder
+        save_object("final_model/model.pkl",best_model)
 
         #Model Trainer Artifact
 
